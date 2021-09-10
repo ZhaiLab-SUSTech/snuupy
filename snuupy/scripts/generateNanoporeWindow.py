00ec938b105172982dad6547a10662fc3397547f
liuzj <liuzj039@gmail.com>
2021-09-10 20:48:33 +0800

    debug

--- snuupy/scripts/generateNanoporeWindow.py
+++ snuupy/scripts/generateNanoporeWindow.py
@@ -73,9 +73,9 @@ def generateNanoporeWindow(GENOME_INDEX, BAM_PATH, OUT_PATH, WINDOW, useColumn, 
     """
     os.system(f'samtools index {BAM_PATH}')
     genomeUpper = getGenomeUpper(GENOME_INDEX, WINDOW, useColumn)
-    with ThreadPoolExecutor(5) as multiT:
+    with ThreadPoolExecutor(2) as multiT:
         sh.mkdir(OUT_PATH, p=True)
         for chr_, upperLimit in genomeUpper.items():
             bamChrFetch = pysam.AlignmentFile(BAM_PATH).fetch(chr_)
             outputPath = f'{OUT_PATH}{chr_}/'
-            multiT.submit(chr_, parseOneChr, bamChrFetch, WINDOW, upperLimit, outputPath, byPrimer)
+            multiT.submit(parseOneChr, chr_, bamChrFetch, WINDOW, upperLimit, outputPath, byPrimer)
